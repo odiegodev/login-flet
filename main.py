a1802fa8724134cb6fc28d4d6be2f4494a442a01
Diego Araujo <diegodev03@gmail.com>
2024-03-09 21:23:48 -0300

    interface completed

--- main.py
+++ main.py
@@ -16,14 +16,78 @@ container = ft.Container(
             ft.TextField(
                 width=200,
                 height=40,
-                hint_text="Digite seu email",
+                hint_text='Digite seu email',
                 border='underline',
                 color='black',
                 prefix_icon= ft.icons.EMAIL
-            )
+            ),
+            padding=ft.padding.only(20,20)
+        ),
+        ft.Container(
+            ft.TextField(
+                width=200,
+                height=40,
+                hint_text='Digite sua senha',
+                border='underline',
+                color='black',
+                prefix_icon= ft.icons.LOCK,
+                password= True
+            ),
+            padding=ft.padding.only(20,20)
+        ),
+        
+        ft.Container(
+            ft.Checkbox(
+                label= 'Salvar senha',
+                check_color='black'   
+            ),
+            padding=ft.padding.only(80)
+        ),
+        ft.Container(
+            ft.ElevatedButton(
+                text='ENTRAR',
+                width=280,
+                bgcolor='black'
+            ),
+            padding=ft.padding.only(20,20)
+        ),
+        ft.Text('Fazer login com',
+                text_align= 'center',
+                width=320),
+        
+        ft.Container(
+            ft.Row([
+                ft.IconButton(
+                    icon= ft.icons.EMAIL,
+                    tooltip='Google',
+                    icon_size=35
+                ),
+                ft.IconButton(
+                    icon= ft.icons.FACEBOOK,
+                    tooltip='Facebook',
+                    icon_size=35
+                ),   
+                ft.IconButton(
+                    icon= ft.icons.APPLE,
+                    tooltip='Apple',
+                    icon_size=35
+                )                  
+            ],
+            alignment= ft.MainAxisAlignment.CENTER
+            ),
+            padding=ft.padding.only(20,20)
+        ),
+        ft.Container(
+            ft.Row([
+                ft.Text('Não tem uma conta?'),
+                ft.TextButton('Criar uma conta')
+            ],
+            alignment= ft.MainAxisAlignment.CENTER
+            ),
+            padding= ft.padding.only(20,20)
         )
-        
-        
+
+ 
     ],
     alignment= ft.MainAxisAlignment.SPACE_EVENLY
     ),
@@ -36,10 +100,10 @@ container = ft.Container(
 )
 
 def main(page: ft.Page):
-    page.bgcolor = ft.colors.BLACK
+    page.bgcolor = ft.colors.BLUE_200
     page.vertical_alignment = 'center'
     page.horizontal_alignment = 'center'
     page.add(container)
 
 
-ft.app(target=main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
